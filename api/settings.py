39ba19430f2591191ca1a04ab7efd4c5dde9a840
georoot <git@rahulbhola.ml>
2017-05-08 15:56:08 +0530

    WIP

--- api/settings.py
+++ api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False if (os.environ['debug'] == 'False') else True
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djangorestframework',
 ]
 
 MIDDLEWARE = [
